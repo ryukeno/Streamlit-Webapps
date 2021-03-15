f5e341782332a94362908cb704d3e77ac3db10b3
ryukeno <55884919+ryukeno@users.noreply.github.com>
2021-03-15 20:11:35 +0100

    Delete eda_app.py

--- eda_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st 
-
-# Load EDA Pkgs
-import pandas as pd
-
-def run_eda_app():
-	st.subheader("From Exploratory Data Analysis")
-	df = pd.read_csv("data/diabetes_data_upload.csv")
-	st.dataframe(df)
